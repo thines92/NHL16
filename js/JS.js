ac714ea6e87e4222d5af45eeccb0e812fffb14ef
Tyler <tylerhines1992@gmail.com>
2015-08-15 14:14:53 -0400

    added if statement to check userTag length

--- js/JS.js
+++ js/JS.js
@@ -1,5 +1,8 @@
 function addTag() {
   userTag = prompt("Please enter your GamerTag").toUpperCase();
+  while (userTag.length <= 1 || userTag.length > 15) {
+    userTag = prompt("Please enter a Gamertag between 1 and 15 characters.").toUpperCase();
+  }
 
   $(".tag-table").append("<tr><td>" + userTag + "</td><td>" + moment().format('MMMM Do YYYY, h:mm:ss a') + "</td></tr>");
 };
